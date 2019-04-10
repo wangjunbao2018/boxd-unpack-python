e9a9b90d5ac27dc5a497b3e27400b545761b91c0
junbao.wang <junbao.wang@castbox.fm>
2019-04-10 17:26:40 +0800

    add class to listen new block

---
+++ boxd_client/boxd_daemon.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from __future__ import print_function
+from __future__ import absolute_import
+
+import threading
+
+from .proto import web_pb2 as web
+
+class BoxdDaemon(threading.Thread):
+
+
+    def __init__(self, box_client, handler):
+        self._web_stub = box_client.web_stub
+        self._handler = handler
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.listen_and_read_new_block(handler=self._handler)
+
+
+    def listen_and_read_new_block(self, handler):
+        blocks = self._web_stub.ListenAndReadNewBlock(web.ListenBlocksReq())
+        try:
+            for block in blocks:
+                handler(block)
+        except:
+            pass
---
+++ sample/boxd_daemon_sample.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from __future__ import print_function
+from __future__ import absolute_import
+
+from boxd_client.boxd_client import BoxdClient
+from boxd_client.boxd_daemon import BoxdDaemon
+
+def block_handler(block):
+    print (block)
+
+boxd_client = BoxdClient("39.105.214.10", 19161)
+boxd_daemon = BoxdDaemon(boxd_client, block_handler)
+boxd_daemon.start()
