649e2ad558ea76f709a32bb8cfcb616657085411
junbao.wang <junbao.wang@castbox.fm>
2019-04-10 16:25:51 +0800

    add method to __init__.py

--- boxd_client/__init__.py
+++ boxd_client/__init__.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+import pkg_resources
+import sys
+import warnings
+
+
+if sys.version_info.major < 3:
+    warnings.simplefilter("always", DeprecationWarning)
+    warnings.warn(
+        DeprecationWarning(
+            "The `boxd-client` library has dropped support for Python 2. Upgrade to Python 3."
+        )
+    )
+    warnings.resetwarnings()
+
+
+__version__ = pkg_resources.get_distribution("boxd-client").version
